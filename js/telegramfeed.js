63362a51db362860fe1e29dfb0dc59af4291c018
produk digital <77826674+produkdigitalhub@users.noreply.github.com>
2026-08-11 14:37:58 +0800

    Update telegramfeed.js

--- js/telegramfeed.js
+++ js/telegramfeed.js
@@ -295,6 +295,11 @@ export async function openTelegramModal(type = 'kajian') {
     }
 }
 
+// Compatibility wrapper used by app.js for the Dashboard menu.
+export async function showTelegramContentInPage(type = 'kajian') {
+    return openTelegramModal(type);
+}
+
 // Inisialisasi Feed
 export function initTelegramFeed() {
     const btnClose = document.getElementById('btn-close-kn-modal');
