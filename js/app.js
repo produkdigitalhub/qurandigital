5758afcdeef120d2a83776182a772d381ec45081
produk digital <77826674+produkdigitalhub@users.noreply.github.com>
2026-08-11 16:22:42 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1625,6 +1625,7 @@ async function openSurahModal(
                     `
                 )
                 .join('');
+        container.scrollTop = 0;
 
 
         // ------------------------------------------------------
